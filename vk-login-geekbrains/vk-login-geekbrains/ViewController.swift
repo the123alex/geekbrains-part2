6bb5f840fbf8cca30f13547e2ddaf4a6d3787d05
Aleksey <alex00836@yandex.ru>
2020-03-31 11:11:00 +0300

    Create project

---
+++ vk-login-geekbrains/vk-login-geekbrains/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  vk-login-geekbrains
+//
+//  Created by Aleksey on 31.03.2020.
+//  Copyright © 2020 Aleksey Mikhlev. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
